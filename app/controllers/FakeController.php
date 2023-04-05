8308aa1746aee79191d9a4f0b90246fe0d7b7c1b
jcheron <myaddressmail@gmail.com>
2023-04-06 00:44:26 +0200

    feat: formatting

--- app/controllers/FakeController.php
+++ app/controllers/FakeController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace controllers;
 
 use Faker\Factory;
@@ -18,60 +19,63 @@ use Ubiquity\orm\DAO;
  * Controller FakeController
  * @property \Ajax\php\ubiquity\JsUtils $jquery
  */
-
 class FakeController extends \controllers\ControllerBase {
 
 	private Generator $faker;
 
 	public function initialize() {
 		parent::initialize();
-		$this->faker=Factory::create('fr_FR');
+		$this->faker = Factory::create('fr_FR');
 		$this->faker->seed(1234);
-		DAO::$useTransformers=true;
+		DAO::$useTransformers = true;
 	}
 
-	protected function createUsers(int $count=30){
-		$users=[];
-		for($i=0;$i<$count;$i++){
-			$user=new \models\User();
+	protected function createUsers(int $count = 30) {
+		$users = [];
+		for ($i = 0; $i < $count; $i++) {
+			$user = new \models\User();
 			$user->setUsername($this->faker->unique()->userName());
 			$user->setEmail($this->faker->unique()->email());
 			$user->setPassword('0000');
 			$user->setCompleteName($this->faker->name);
-			$users[]=$user;
+			$users[] = $user;
 			DAO::insert($user);
 		}
 		return $users;
 	}
 
-	protected function createSuites(){
-		$fibonacci=new Suite();
+	protected function createSuites() {
+		$fibonacci = new Suite();
+		$fibonacci->setPublic(true);
 		$fibonacci->setName('Fibonacci');
 		$fibonacci->setSuitevalues('[0,1,3,5,8,13,21,34,55,89]');
 		DAO::insert($fibonacci);
-		$teaShirtSizes=new Suite();
+		$teaShirtSizes = new Suite();
+		$teaShirtSizes->setPublic(true);
 		$teaShirtSizes->setName('T-shirt');
 		$teaShirtSizes->setSuitevalues('["XS","S","M","L","XL","XXL"]');
 		DAO::insert($teaShirtSizes);
-		$scrumSuite=new Suite();
+		$scrumSuite = new Suite();
+		$scrumSuite->setPublic(true);
 		$scrumSuite->setName('Scrum');
 		$scrumSuite->setSuitevalues('[0,0.5,1,2,3,5,8,13,20,40,100]');
 		DAO::insert($scrumSuite);
-		$playingCards=new Suite();
+		$playingCards = new Suite();
+		$playingCards->setPublic(true);
 		$playingCards->setName('Playing cards');
 		$playingCards->setSuitevalues('["2","3","4","5","6","7","8","9","10","J","Q","K","A"]');
 		DAO::insert($playingCards);
 	}
 
-	protected function createConfigurations(){
-		$paramsNames=['hasObservers'=>'Request confirmation when skipping stories?',
-			'skipConfirmation'=>'Request confirmation when skipping stories?',
-			'autoReveal'=>'Reveal cards automatically?',
-			'changeVote'=>'Allow changing vote?',
-			'countDownTimer'=>'Enable countdown timer?'
+	protected function createConfigurations() {
+		$paramsNames = ['hasObservers' => 'Request confirmation when skipping stories?',
+			'skipConfirmation' => 'Request confirmation when skipping stories?',
+			'autoReveal' => 'Reveal cards automatically?',
+			'changeVote' => 'Allow changing vote?',
+			'countDownTimer' => 'Enable countdown timer?'
 		];
-		foreach($paramsNames as $name=>$description){
-			$params=new Params();
+		foreach ($paramsNames as $name => $description) {
+			$params = new Params();
 			$params->setName($name);
 			$params->setDescription($description);
 			DAO::toInsert($params);
@@ -79,36 +83,36 @@ class FakeController extends \controllers\ControllerBase {
 		DAO::flushInserts();
 	}
 
-	protected function getRandomInstance(string $model,$include=true){
-		return DAO::getOne($model,'1=1 order by rand()',$include);
+	protected function getRandomInstance(string $model, $include = true) {
+		return DAO::getOne($model, '1=1 order by rand()', $include);
 	}
 
-	protected function getRandomInstances(string $model,int $count){
-		return DAO::getAll($model,'1=1 order by rand() limit '.$count);
+	protected function getRandomInstances(string $model, int $count) {
+		return DAO::getAll($model, '1=1 order by rand() limit ' . $count);
 	}
 
-	protected function createStories(Room $room,int $count):array{
-		$stories=[];
-		for($i=0;$i<$count;$i++){
-			$story=new Story();
+	protected function createStories(Room $room, int $count): array {
+		$stories = [];
+		for ($i = 0; $i < $count; $i++) {
+			$story = new Story();
 			$story->setName($this->faker->unique()->sentence(5));
 			$story->setRoom($room);
 			$story->setDescription($this->faker->paragraph());
-			$stories[]=$story;
+			$stories[] = $story;
 			DAO::insert($story);
 		}
 		return $stories;
 	}
 
-	protected function getRandomValueInSuite(Suite $suite){
-		$values=\explode(',',$suite->getSuitevalues());
+	protected function getRandomValueInSuite(Suite $suite) {
+		$values = \explode(',', $suite->getSuitevalues());
 		return $values[\array_rand($values)];
 	}
 
-	protected function createVotes(array $stories,array $users,Suite $suite){
-		foreach($stories as $story){
-			foreach($users as $user){
-				$v=new Voter();
+	protected function createVotes(array $stories, array $users, Suite $suite) {
+		foreach ($stories as $story) {
+			foreach ($users as $user) {
+				$v = new Voter();
 				$v->setUser($user);
 				$v->setStory($story);
 				$v->setPoints($this->getRandomValueInSuite($suite));
@@ -117,41 +121,41 @@ class FakeController extends \controllers\ControllerBase {
 		}
 	}
 
-	protected function createRoomAndStories(int $count=10,int $nbStories=20,bool $createVotes=false){
-		for($i=0;$i<$count;$i++){
-			$room=new Room();
+	protected function createRoomAndStories(int $count = 10, int $nbStories = 20, bool $createVotes = false) {
+		for ($i = 0; $i < $count; $i++) {
+			$room = new Room();
 			$room->setName($this->faker->unique()->sentence(1));
 			$room->setUser($this->getRandomInstance(User::class));
-			$suite=$this->getRandomInstance(Suite::class);
+			$suite = $this->getRandomInstance(Suite::class);
 			$room->setSuite($suite);
-			$team=$this->getRandomInstance(Team::class);
+			$team = $this->getRandomInstance(Team::class);
 			$room->setTeam($team);
-			DAO::insert($room,true);
-			$stories=$this->createStories($room,$nbStories);
-			if($createVotes) {
-				$this->createVotes($stories,$team->getUsers(),$suite);
+			DAO::insert($room, true);
+			$stories = $this->createStories($room, $nbStories);
+			if ($createVotes) {
+				$this->createVotes($stories, $team->getUsers(), $suite);
 			}
 		}
 	}
 
-	protected function createTeams(int $count=10,int $nbUsers=10){
-		for($i=0;$i<$count;$i++){
-			$team=new Team();
+	protected function createTeams(int $count = 10, int $nbUsers = 10) {
+		for ($i = 0; $i < $count; $i++) {
+			$team = new Team();
 			$team->setName($this->faker->unique()->sentence(1));
 			$team->setUser($this->getRandomInstance(User::class));
-			$team->setUsers($this->getRandomInstances(User::class,$nbUsers));
-			DAO::insert($team,true);
+			$team->setUsers($this->getRandomInstances(User::class, $nbUsers));
+			DAO::insert($team, true);
 		}
 	}
 
 	#[Route('_fake')]
-	public function index(){
-		$data=['nbUsers'=>50,'nbRooms'=>5,'nbTeams'=>5,'nbStories'=>20,'hasSuites'=>DAO::count(Suite::class)==0,
-			'hasConfig'=>DAO::count(Params::class)==0,'nbUsersTeam'=>10,'nbStoriesRoom'=>20];
-		$this->loadView('/FakeController/index.html',$data);
+	public function index() {
+		$data = ['nbUsers' => 50, 'nbRooms' => 5, 'nbTeams' => 5, 'nbStories' => 20, 'hasSuites' => DAO::count(Suite::class) == 0,
+			'hasConfig' => DAO::count(Params::class) == 0, 'nbUsersTeam' => 10, 'nbStoriesRoom' => 20];
+		$this->loadView('/FakeController/index.html', $data);
 	}
 
-	#[Post('_fake/create',name:'fake.create')]
+	#[Post('_fake/create', name: 'fake.create')]
 	public function createFakeAction() {
 		$nbUsers = $_POST['nbUsers'] ?? 50;
 		$nbRooms = $_POST['nbRooms'] ?? 5;
@@ -182,6 +186,5 @@ class FakeController extends \controllers\ControllerBase {
 		$list->addItem('Configurations created: ' . DAO::count(Params::class));
 		$this->jquery->renderView('FakeController/createFakeAction.html');
 	}
-
 
 }
--- app/models/Room.php
+++ app/models/Room.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace models;
 
 use Ubiquity\attributes\items\Id;
@@ -11,227 +12,227 @@ use Ubiquity\attributes\items\JoinColumn;
 
 #[\AllowDynamicProperties()]
 #[Table(name: "room")]
-class Room{
-	
+class Room {
+
 	#[Id()]
-	#[Column(name: "id",dbType: "int(11)")]
-	#[Validator(type: "id",constraints: ["autoinc"=>true])]
+	#[Column(name: "id", dbType: "int(11)")]
+	#[Validator(type: "id", constraints: ["autoinc" => true])]
 	private $id;
 
-	
-	#[Column(name: "name",dbType: "varchar(100)")]
-	#[Validator(type: "length",constraints: ["max"=>"100","notNull"=>true])]
+
+	#[Column(name: "name", dbType: "varchar(100)")]
+	#[Validator(type: "length", constraints: ["max" => "100", "notNull" => true])]
 	private $name;
 
-	
-	#[Column(name: "description",dbType: "text")]
-	#[Validator(type: "notNull",constraints: [])]
+
+	#[Column(name: "description", dbType: "text")]
+	#[Validator(type: "notNull", constraints: [])]
 	private $description;
 
-	
-	#[Column(name: "points",dbType: "tinyint(4)")]
-	#[Validator(type: "notNull",constraints: [])]
+
+	#[Column(name: "points", dbType: "tinyint(4)")]
+	#[Validator(type: "notNull", constraints: [])]
 	private $points;
 
-    #[Column(name: "uuid",dbType: "varchar(100)")]
-    private $uuid;
+	#[Column(name: "uuid", dbType: "varchar(100)")]
+	private $uuid;
 
-	
-	#[Column(name: "connectedUsers",dbType: "text")]
-	#[Validator(type: "notNull",constraints: [])]
-	private $connectedUsers='[]';
 
-	
-	#[OneToMany(mappedBy: "room",className: "models\\Configuration")]
+	#[Column(name: "connectedUsers", dbType: "text")]
+	#[Validator(type: "notNull", constraints: [])]
+	private $connectedUsers = '[]';
+
+
+	#[OneToMany(mappedBy: "room", className: "models\\Configuration")]
 	private $configurations;
 
-	
-	#[OneToMany(mappedBy: "room",className: "models\\Permission")]
+
+	#[OneToMany(mappedBy: "room", className: "models\\Permission")]
 	private $permissions;
 
-	
-	#[OneToMany(mappedBy: "room",className: "models\\Story")]
+
+	#[OneToMany(mappedBy: "room", className: "models\\Story")]
 	private $storys;
 
-	
+
 	#[ManyToOne()]
-	#[JoinColumn(className: "models\\Suite",name: "idSuite")]
+	#[JoinColumn(className: "models\\Suite", name: "idSuite")]
 	private $suite;
 
-	
+
 	#[ManyToOne()]
-	#[JoinColumn(className: "models\\Team",name: "idTeam")]
+	#[JoinColumn(className: "models\\Team", name: "idTeam")]
 	private $team;
 
-	
+
 	#[ManyToOne()]
-	#[JoinColumn(className: "models\\User",name: "idOwner")]
+	#[JoinColumn(className: "models\\User", name: "idOwner")]
 	private $user;
 
 
-	 public function __construct(){
+	public function __construct() {
 		$this->configurations = [];
 		$this->permissions = [];
 		$this->storys = [];
 	}
 
 
-	public function getId(){
+	public function getId() {
 		return $this->id;
 	}
 
 
-	public function setId($id){
-		$this->id=$id;
+	public function setId($id) {
+		$this->id = $id;
 	}
 
 
-	public function getName(){
+	public function getName() {
 		return $this->name;
 	}
 
 
-	public function setName($name){
-		$this->name=$name;
+	public function setName($name) {
+		$this->name = $name;
 	}
 
 
-	public function getDescription(){
+	public function getDescription() {
 		return $this->description;
 	}
 
 
-	public function setDescription($description){
-		$this->description=$description;
+	public function setDescription($description) {
+		$this->description = $description;
 	}
 
 
-	public function getPoints(){
+	public function getPoints() {
 		return $this->points;
 	}
 
 
-	public function setPoints($points){
-		$this->points=$points;
+	public function setPoints($points) {
+		$this->points = $points;
 	}
 
 
-	public function getConnectedUsers(){
+	public function getConnectedUsers() {
 		return $this->connectedUsers;
 	}
 
 
-	public function setConnectedUsers($connectedUsers){
-		$this->connectedUsers=$connectedUsers;
+	public function setConnectedUsers($connectedUsers) {
+		$this->connectedUsers = $connectedUsers;
 	}
 
 
-	public function getConfigurations(){
+	public function getConfigurations() {
 		return $this->configurations;
 	}
 
 
-	public function setConfigurations($configurations){
-		$this->configurations=$configurations;
+	public function setConfigurations($configurations) {
+		$this->configurations = $configurations;
 	}
 
 
-	 public function addToConfigurations($configuration){
-		$this->configurations[]=$configuration;
+	public function addToConfigurations($configuration) {
+		$this->configurations[] = $configuration;
 		$configuration->setRoom($this);
 	}
 
 
-	public function getPermissions(){
+	public function getPermissions() {
 		return $this->permissions;
 	}
 
 
-	public function setPermissions($permissions){
-		$this->permissions=$permissions;
+	public function setPermissions($permissions) {
+		$this->permissions = $permissions;
 	}
 
 
-	 public function addToPermissions($permission){
-		$this->permissions[]=$permission;
+	public function addToPermissions($permission) {
+		$this->permissions[] = $permission;
 		$permission->setRoom($this);
 	}
 
 
-	public function getStorys(){
+	public function getStorys() {
 		return $this->storys;
 	}
 
 
-	public function setStorys($storys){
-		$this->storys=$storys;
+	public function setStorys($storys) {
+		$this->storys = $storys;
 	}
 
 
-	 public function addToStorys($tory){
-		$this->storys[]=$tory;
+	public function addToStorys($tory) {
+		$this->storys[] = $tory;
 		$tory->setRoom($this);
 	}
 
 
-	public function getSuite(){
+	public function getSuite() {
 		return $this->suite;
 	}
 
 
-	public function setSuite($suite){
-		$this->suite=$suite;
+	public function setSuite($suite) {
+		$this->suite = $suite;
 	}
 
 
-	public function getTeam(){
+	public function getTeam() {
 		return $this->team;
 	}
 
 
-	public function setTeam($team){
-		$this->team=$team;
+	public function setTeam($team) {
+		$this->team = $team;
 	}
 
 
-	public function getUser(){
+	public function getUser() {
 		return $this->user;
 	}
 
 
-	public function setUser($user){
-		$this->user=$user;
+	public function setUser($user) {
+		$this->user = $user;
 	}
 
-    /**
-     * @param mixed $uuid
-     */
-    public function setUuid($uuid): void{
-        $this->uuid = $uuid;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getUuid() {
-        return $this->uuid;
-    }
-
-	 public function __toString(){
-		return ($this->name??'no value').'';
+	/**
+	 * @param mixed $uuid
+	 */
+	public function setUuid($uuid): void {
+		$this->uuid = $uuid;
 	}
 
-    public function addConnectedUser(User $user): void{
-         $connectedUsers=\json_decode($this->connectedUsers,true);
-         $connectedUsers[]=$user->_rest;
-        $this->connectedUsers=json_encode($connectedUsers);
-    }
-    public function removeConnectedUser(User $user): void{
-         $connectedUsers=\json_decode($this->connectedUsers,true);
-         $connectedUsers=\array_filter($connectedUsers,function($u) use ($user){
-			 return $u['id']!=$user->getId();
-		 });
-        $this->connectedUsers=\json_encode($connectedUsers);
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getUuid() {
+		return $this->uuid;
+	}
 
+	public function __toString() {
+		return ($this->name ?? 'no value') . '';
+	}
+
+	public function addConnectedUser(User $user): void {
+		$connectedUsers = \json_decode($this->connectedUsers, true);
+		$connectedUsers[] = $user->_rest;
+		$this->connectedUsers = json_encode($connectedUsers);
+	}
+
+	public function removeConnectedUser(User $user): void {
+		$connectedUsers = \json_decode($this->connectedUsers, true);
+		$connectedUsers = \array_filter($connectedUsers, function ($u) use ($user) {
+			return $u['id'] != $user->getId();
+		});
+		$this->connectedUsers = \json_encode(\array_values($connectedUsers));
+	}
 }
